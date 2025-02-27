7feb402af5d0c4b9e5087bf461d03ad99de08a04
thaydeveloper <thaydeveloper26@gmail.com>
2025-02-27 18:10:54 -0300

    feat(ci/cd): add ci cd and project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+  },
 ];
 
 export default eslintConfig;
