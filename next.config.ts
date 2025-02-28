36efd26de17245b6a520e87995c51bed594a0b91
thaydeveloper <thaydeveloper26@gmail.com>
2025-02-27 21:26:42 -0300

    fix: config deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'build',
+  // Para GitHub Pages com repositório que não está na raiz:
+  // basePath: '/nome-do-repositorio',
+  // assetPrefix: '/nome-do-repositorio/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
